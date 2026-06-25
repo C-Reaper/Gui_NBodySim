03ae16b18aa855b6ce62e5a766f9a1bab685aa86
codeleaded <gnu.c.reaper@gmail.com>
2026-06-26 01:29:58 +0200

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,43 +1,63 @@
 #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
-#include "/home/codeleaded/System/Static/Library/TransformedView.h"
-#include "/home/codeleaded/System/Static/Library/LSystem.h"
+#include "/home/codeleaded/System/Static/Library/Random.h"
+#include "/home/codeleaded/System/Static/Library/NBodySim.h"
 
 
-#define NBODYS  100000U
-TransformedView tv;
+NBodySim nbs;
 
 void Setup(AlxWindow* w){
-	tv = TransformedView_Make(
-		(Vec2){ GetWidth(),GetHeight() },
-		(Vec2){ 0.0f,0.0f },
-		(Vec2){ 0.1f,0.1f },
-		(float)GetWidth() / (float)GetHeight()
-	);
+	nbs = NBodySim_New(1000U,1000U);
     AlxFont_Resize(&window.font,50,50);
-
-    
 }
 
 void Update(AlxWindow* w){
-    tv.ZoomSpeed = (float)w->ElapsedTime;
-	TransformedView_HandlePanZoom(&tv,window.Strokes,(Vec2){ GetMouse().x,GetMouse().y });
-	TransformedView_Output(&tv,(Vec2){ GetWidth(),GetHeight() });
-    
-    
+    NBodySim_Input(&nbs,w->Strokes,GetMouse());
+
+	if(Stroke(ALX_MOUSE_L).DOWN){
+		const Vec2 velo = TransformedView_ScreenWorldLength(&nbs.tv,Vec2_Sub(GetMouse(),GetMouseBefore()));
+		const Vec2 pm = TransformedView_ScreenWorldPos(&nbs.tv,GetMouse());
+		const float r = (float)Random_f64_MinMax(0.5f,1.0f);
+		
+		NBody nb = NBody_New(
+			WHITE,
+			pm.x,
+			pm.y,
+			r * 10.0f,
+			r
+		);
+		nb.vx = velo.x * 3.0f;
+		nb.vy = velo.y * 3.0f;
+
+		NBodySim_Add(&nbs,&nb);
+	}
+	if(Stroke(ALX_MOUSE_R).PRESSED){
+		const Vec2 pm = TransformedView_ScreenWorldPos(&nbs.tv,GetMouse());
+		const float r = 30.0f;//(float)Random_f64_MinMax(10.0f,11.0f);
+
+		NBodySim_Add(&nbs,(NBody[]){
+			NBody_New(
+				WHITE,
+				pm.x,
+				pm.y,
+				r * 1000.0f,
+				r
+			)
+		});
+	}
+
+    NBodySim_Update(&nbs,w->ElapsedTime);
 
 	Clear(BLACK);
 
-    
-    
-    CStr_RenderAlxFontf(WINDOW_STD_ARGS,GetAlxFont(),0.0f,0.0f,RED,"B: %u",NBODYS);
+    NBodySim_Render(&nbs,WINDOW_STD_ARGS);
 }
 
 void Delete(AlxWindow* w){
-    
+    NBodySim_Free(&nbs);
 }
 
 int main(){
-    if(Create("N-Body-Simulation",1800,1000,1,1,Setup,Update,Delete))
+    if(Create("N-Body-Simulation",1900,1100,1,1,Setup,Update,Delete))
         Start();
     return 0;
 }
